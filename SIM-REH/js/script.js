8b5dd753012ed163e837ce862d9845e97f04848c
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2021-12-15 18:18:48 +0100

    Fixed contact for good, fixed scroll-spy for good and added section rehabilitation

--- SIM-REH/js/script.js
+++ SIM-REH/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const nav = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navBrand = document.querySelectorAll('.navbar-brand')
+    const navList = document.querySelector('.navbar-collapse')
 
     function addShadow() {
         if (window.scrollY >= 300) {
@@ -9,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function () {
             nav.classList.remove('shadow-bg')
         }
     }
+
+    
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+    navBrand.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+    
+    
 
     window.addEventListener('scroll', addShadow)
 })
---
+++ SIM-REH/js/slick_settings.js
@@ -0,0 +1,25 @@
+$('.rehabilitation-carousel').slick({
+
+    mobileFirst: true,
+    autoplay: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3
+            }
+        }
+    ]
+
+
+});
